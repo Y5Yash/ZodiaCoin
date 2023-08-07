aa546538424c2a0230557bb56bbeb6f1501280b4
Yash <yash.chaurasia@research.iiit.ac.in>
2023-08-07 12:27:30 +0530

    update links in navbar

--- frontend/src/components/navbar.component.tsx
+++ frontend/src/components/navbar.component.tsx
@@ -4,8 +4,8 @@ export const Navbar =({walletAddr}: {walletAddr: string})=>{
     return(
         <div className="navbar-class">
            <div className="navbar-coins">
-            <div><a href="#">Astrocoin</a></div>
-            <div><a href="#">Zodiacoin</a></div>
+            <div><a href="https://astrocoin.onrender.com">Astrocoin</a></div>
+            <div><a href="https://zodiacoin.onrender.com">Zodiacoin</a></div>
            </div>
            <div className="navbar-wallet">
             <div>{walletAddr}</div>
